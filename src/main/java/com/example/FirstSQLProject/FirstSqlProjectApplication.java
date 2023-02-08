43b3e25c6c6da6bae330439192e191a110393f83
krunalSolanki1 <krunalsolucky121@gmail.com>
2023-02-08 15:48:58 +0530

    initial

---
+++ src/main/java/com/example/FirstSQLProject/FirstSqlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstSQLProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSqlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSqlProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/FirstSQLProject/UserRepositoryy.java
@@ -0,0 +1,7 @@
+package com.example.FirstSQLProject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepositoryy extends JpaRepository<User, Integer> {
+
+}
